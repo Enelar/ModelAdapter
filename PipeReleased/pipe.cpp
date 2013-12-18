b67ca586d393cb6dd48fdeb400086c4f2336c6c3
Kirill Berezin <enelar@develop-project.ru>
2013-12-18 15:12:16 +0400

    Concrete serializer resolution

--- PipeReleased/pipe.cpp
+++ PipeReleased/pipe.cpp
@@ -55,9 +55,70 @@ word pipe::GetID(const OBJECT_TYPES type, const std::string &name)
   return ep->GetID(type, name.c_str());
 }
 
+#include <tuple>
+template<typename T>
+bool Serializator(const server_object_container<typename T::mapped>, vector<param> &)
+{
+  IMPLEMENTATION_REQUIRED
+}
+
+
+template<class Tuple, int level = -1>
+struct TestSerializator
+{
+  vector<param> operator()(const server_object_container<CShBase> obj, OBJECT_TYPES type)
+  {
+    static_assert(level >= 0, "Wrong template resolution");
+    const int tuple_size = std::tuple_size<Tuple>::value;
+
+    typedef std::tuple_element<level - 1, Tuple>::type selected_type;
+    const int obj_id = object_id<selected_type>::tid;
+
+    if (obj_id != type)
+      return [=]()
+      {
+        TestSerializator<Tuple, level - 1> t;
+        return t(obj, type);
+      }();
+
+    vector<param> ret;
+    throw_assert(Serializator<selected_type>(obj, ret));
+    return ret;
+  }
+};
+
+template<class Tuple>
+struct TestSerializator<Tuple, -1>
+{
+  vector<param> operator()(const server_object_container<CShBase> data, OBJECT_TYPES type)
+  {
+    const int size = std::tuple_size<Tuple>::value;
+    TestSerializator<Tuple, size> t;
+    return t(data, type);
+  }
+};
+
+template<class Tuple>
+struct TestSerializator<Tuple, 0>
+{
+  vector<param> operator()(const server_object_container<CShBase> data, OBJECT_TYPES type)
+  {
+    return{};
+  }
+};
+
 vector<param> pipe::GetRaw(const object_interface &obj)
 {
-  implementation_required("Get full object information from model. Exported pipe needed");
+  auto server_object = ep->GetServerObject(obj.id);
+  typedef std::tuple<objects::valve> supported_server_objects;
+  supported_server_objects t;
+  
+  auto ret = [=]()
+  {
+    TestSerializator<supported_server_objects> t;
+    return t(server_object, obj.type);
+  }();
+  return ret;
 }
 
 void pipe::SetRaw(const object_interface &obj, vector<param> data)
